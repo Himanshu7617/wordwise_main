d84d35992c9b6a993e536c7ffe40acb2f52e6c05
Himanshu <gghimanshu333@gmail.com>
2025-04-29 18:57:47 +0530

    created env file in extension frontend

--- wordwise_extension/extension_frontend/webpack.common.ts
+++ wordwise_extension/extension_frontend/webpack.common.ts
@@ -1,6 +1,9 @@
 import path from 'path'
 import webpack, { DefinePlugin } from 'webpack' 
 import CopyWebpackPlugin from 'copy-webpack-plugin'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 const config: webpack.Configuration = {
@@ -50,13 +53,13 @@ const config: webpack.Configuration = {
             patterns: [{from : 'static'}],
         }),
         new DefinePlugin({
-            "process.env.API_KEY" : JSON.stringify('AIzaSyBU1pOlg1RXIGqYbE1_uMWwAa_9WB3Gcpw'),
-            "process.env.FIREBASE_API_KEY" : JSON.stringify("AIzaSyDD_n0qhvKuorl6YwJyIy9ObgfcUX3x6bE"),
-            "process.env.FIREBASE_AUTH_DOMAIN" : JSON.stringify("fluentify-7a78a.firebaseapp.com"),
-            "process.env.FIREBASE_PROJECT_ID" : JSON.stringify("fluentify-7a78a"),
-            "process.env.FIREBASE_STORAGE_BUCKET" : JSON.stringify("fluentify-7a78a.firebasestorage.app"),
-            "process.env.FIREBASE_SENDER_ID" : JSON.stringify("1096214608573"),
-            "process.env.FIREBASE_APP_ID" : JSON.stringify("1:1096214608573:web:e947b22b3542da8ba695d4")
+            "process.env.API_KEY" : JSON.stringify(process.env.API_KEY),
+            "process.env.FIREBASE_API_KEY" : JSON.stringify(process.env.FIREBASE_API_KEY),
+            "process.env.FIREBASE_AUTH_DOMAIN" : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+            "process.env.FIREBASE_PROJECT_ID" : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+            "process.env.FIREBASE_STORAGE_BUCKET" : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+            "process.env.FIREBASE_SENDER_ID" : JSON.stringify(process.env.FIREBASE_SENDER_ID),
+            "process.env.FIREBASE_APP_ID" : JSON.stringify(process.env.FIREBASE_APP_ID)
         })
     ]
 }
